c79498887cea0257f7d8b24508bccb242c185fe0
amit.sahoo <amit.sahoo@cgi.com>
2018-05-02 14:16:06 +0530

    Chaged to get the DB1 to Db2 Work

--- src/main/java/com/infotech/batch/config/BatchConfiguration.java
+++ src/main/java/com/infotech/batch/config/BatchConfiguration.java
@@ -38,39 +38,10 @@ public class BatchConfiguration {
     public DataSource dataSource;
 
     @Bean
-    public FlatFileItemReader<Person> reader() {
-        FlatFileItemReader<Person> reader = new FlatFileItemReader<Person>();
-        reader.setResource(new ClassPathResource("persons.csv"));
-        reader.setLineMapper(new DefaultLineMapper<Person>() {{
-            setLineTokenizer(new DelimitedLineTokenizer() {{
-                setNames(new String[] { "firstName", "lastName","email","age" });
-            }});
-            setFieldSetMapper(new BeanWrapperFieldSetMapper<Person>() {{
-                setTargetType(Person.class);
-            }});
-        }});
-        return reader;
-    }
-
-    @Bean
-    public PersonItemProcessor processor() {
-        return new PersonItemProcessor();
-    }
-
-    @Bean
-    public JdbcBatchItemWriter<Person> writer() {
-        JdbcBatchItemWriter<Person> writer = new JdbcBatchItemWriter<Person>();
-        writer.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<Person>());
-        writer.setSql("INSERT INTO person (first_name, last_name,email,age) VALUES (:firstName, :lastName,:email,:age)");
-        writer.setDataSource(dataSource);
-        return writer;
-    }
-
-    @Bean
 	public JdbcCursorItemReader<Person> readerStep2(){
 		JdbcCursorItemReader<Person> cursorItemReader = new JdbcCursorItemReader<>();
 		cursorItemReader.setDataSource(dataSource);
-		cursorItemReader.setSql("SELECT person_id,first_name,last_name,email,age FROM person");
+		cursorItemReader.setSql("SELECT person_id,first_name,last_name,email,age FROM person limit 1");
 		cursorItemReader.setRowMapper(new PersonRowMapper());
 		return cursorItemReader;
 	}
@@ -82,39 +53,26 @@ public class BatchConfiguration {
     public JdbcBatchItemWriter<Person> writerStep2() {
         JdbcBatchItemWriter<Person> writer = new JdbcBatchItemWriter<Person>();
         writer.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<Person>());
-        writer.setSql("INSERT INTO person1 (first_name, last_name,email,age) VALUES (:firstName, :lastName,:email,:age)");
+        writer.setSql("INSERT INTO person2 (first_name, last_name,email,age) VALUES (:firstName, :lastName,:email,:age)");
         writer.setDataSource(dataSource);
         return writer;
     }
     
     @Bean
-    public Job importUserJob() {
-        return jobBuilderFactory.get("importUserJob")
+    public Job importUserJob1() {
+        return jobBuilderFactory.get("importUserJob1")
                 .incrementer(new RunIdIncrementer())
-                //.listener(listener)
                 .flow(step2())
-                /*.start(step1())
-                .next(step2())*/
                 .end()
                 .build();
     }
-
-    @Bean
-    public Step step1() {
-    	System.out.println("In Step 1");
-        return stepBuilderFactory.get("step1")
-                .<Person, Person> chunk(chunkSize)
-                .reader(reader())
-                .processor(processor())
-                .writer(writer())
-                .build();
-    }
-    static int chunkSize = 100;
+    
+    
     @Bean
     public Step step2() {
     	System.out.println("In Step 2");
         return stepBuilderFactory.get("step2")
-                .<Person, Person> chunk(chunkSize)
+                .<Person, Person> chunk(100)
                 .reader(readerStep2())
                 .processor(processorStep2())
                 .writer(writerStep2())
